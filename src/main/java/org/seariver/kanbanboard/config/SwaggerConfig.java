6133ce1958137bd70f9d6b35d3a4bbc0b663bf76
Mario Rezende <mariorez@gmail.com>
2020-06-13 23:59:53 -0300

    api documentation

---
+++ src/main/java/org/seariver/kanbanboard/config/SwaggerConfig.java
@@ -0,0 +1,19 @@
+package org.seariver.kanbanboard.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+            .select()
+            .build();
+    }
+}
