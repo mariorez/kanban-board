7df9c38a78b2d85e5e7419d822204ebdaf9ee976
Mario Rezende <mariorez@gmail.com>
2020-06-20 23:58:55 -0300

    reset fixtures for running multiples tests

--- src/main/java/org/seariver/kanbanboard/write/domain/application/CreateBucketCommand.java
+++ src/main/java/org/seariver/kanbanboard/write/domain/application/CreateBucketCommand.java
@@ -2,14 +2,14 @@ package org.seariver.kanbanboard.write.domain.application;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
-import javax.validation.constraints.Max;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Positive;
+import javax.validation.constraints.Size;
 import java.util.UUID;
 
 public record CreateBucketCommand(
     @NotNull @JsonProperty("id")UUID id,
     @Positive @JsonProperty("position")double position,
-    @NotBlank @Max(100) @JsonProperty("name")String name) implements Command {
+    @NotBlank @Size(min = 1, max = 100) @JsonProperty("name")String name) implements Command {
 }
