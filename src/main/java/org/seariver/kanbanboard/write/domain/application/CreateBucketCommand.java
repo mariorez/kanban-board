5e0e8a8155730828451f5ed3df363bfcc3f79a54
Mario Rezende <mariorez@gmail.com>
2020-06-20 23:31:45 -0300

    test scenario to validate bucket creation

--- src/main/java/org/seariver/kanbanboard/write/domain/application/CreateBucketCommand.java
+++ src/main/java/org/seariver/kanbanboard/write/domain/application/CreateBucketCommand.java
@@ -2,13 +2,14 @@ package org.seariver.kanbanboard.write.domain.application;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+import javax.validation.constraints.Max;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Positive;
 import java.util.UUID;
 
 public record CreateBucketCommand(
-    @NotNull @JsonProperty("id") UUID id,
-    @Positive @JsonProperty("position") double position,
-    @NotBlank @JsonProperty("name") String name) implements Command {
+    @NotNull @JsonProperty("id")UUID id,
+    @Positive @JsonProperty("position")double position,
+    @NotBlank @Max(100) @JsonProperty("name")String name) implements Command {
 }
--- src/test/java/org/seariver/kanbanboard/write/adapter/in/WriteBucketControllerIT.java
+++ src/test/java/org/seariver/kanbanboard/write/adapter/in/WriteBucketControllerIT.java
@@ -10,15 +10,72 @@ import java.util.UUID;
 import static org.hamcrest.Matchers.containsInRelativeOrder;
 import static org.springframework.http.MediaType.APPLICATION_JSON;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@Disabled("Waiting full implementation")
 @TestPropertySource(properties = "test.dataset=WriteBucketControllerIT")
 class WriteBucketControllerIT extends IntegrationHelper {
 
+    @Test
+    void GIVEN_ValidData_MUST_CreateBucket() throws Exception {
+        // given
+        var id = UUID.randomUUID();
+        var position = faker.number().randomDouble(5, 1, 10);
+        var name = faker.pokemon().name();
+        var payload = """
+            {
+                "id": "%s",
+                "position": %s,
+                "name": "%s"
+            }
+            """.formatted(id, position, name);
+
+        // when
+        mockMvc
+            .perform(post("/v1/buckets")
+                .contentType("application/json")
+                .content(payload))
+            .andExpect(status().isCreated());
+
+        // then
+        mockMvc
+            .perform(get("/v1/buckets"))
+            .andExpect(status().isOk())
+            .andExpect(content().contentType(APPLICATION_JSON))
+            .andExpect(jsonPath("$[*].id",
+                containsInRelativeOrder(id.toString(), "6d9db741-ef57-4d5a-ac0f-34f68fb0ab5e", "3731c747-ea27-42e5-a52b-1dfbfa9617db")))
+            .andExpect(jsonPath("$[*].position",
+                containsInRelativeOrder(position, 100.15, 200.987)))
+            .andExpect(jsonPath("$[*].name", containsInRelativeOrder(name, "FIRST-BUCKET", "SECOND-BUCKET")));
+    }
+
+    @Test
+    void GIVEN_InvalidData_MUST_ReturnError() throws Exception {
+        // given
+        var id = UUID.randomUUID();
+        var position = faker.number().randomDouble(5, 1, 10);
+        var name = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras fringilla elit elementum, ullamcorper turpis consequat.";
+        var payload = """
+            {
+                "id": "%s",
+                "position": %s,
+                "name": "%s"
+            }
+            """.formatted(id, position, name);
+
+        // when
+        mockMvc
+            .perform(post("/v1/buckets")
+                .contentType("application/json")
+                .content(payload))
+            .andExpect(status().isBadRequest());
+    }
+
+
+    @Disabled("Waiting full implementation")
     @Test
     void GIVEN_ValidData_MUST_UpdateBucket() throws Exception {
 
@@ -28,8 +85,8 @@ class WriteBucketControllerIT extends IntegrationHelper {
         var name = faker.pokemon().name();
         var payload = """
             {
-                "position" = %s,
-                "name" = "%s"
+                "position": %s,
+                "name": "%s"
             }
             """.formatted(position, name);
 
@@ -46,7 +103,7 @@ class WriteBucketControllerIT extends IntegrationHelper {
             .andExpect(status().isOk())
             .andExpect(content().contentType(APPLICATION_JSON))
             .andExpect(jsonPath("$[*].id", containsInRelativeOrder(id, "6d9db741-ef57-4d5a-ac0f-34f68fb0ab5e")))
-            .andExpect(jsonPath("$[*].positon", containsInRelativeOrder(position, 100.15)))
+            .andExpect(jsonPath("$[*].position", containsInRelativeOrder(position, 100.15)))
             .andExpect(jsonPath("$[*].name", containsInRelativeOrder(name, "FIRST-BUCKET")));
     }
 }
