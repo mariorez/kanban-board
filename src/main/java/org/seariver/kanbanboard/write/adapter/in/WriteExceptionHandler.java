f149325494463f5dcc060a24936ce7fdd2298050
Mario Rezende <mariorez@gmail.com>
2020-07-01 23:30:27 -0300

    configure log saas

--- src/main/java/org/seariver/kanbanboard/write/adapter/in/WriteExceptionHandler.java
+++ src/main/java/org/seariver/kanbanboard/write/adapter/in/WriteExceptionHandler.java
@@ -7,8 +7,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 
 import javax.validation.ConstraintViolationException;
@@ -19,7 +19,7 @@ import java.util.stream.Collectors;
 
 import static org.springframework.http.HttpStatus.BAD_REQUEST;
 
-@ControllerAdvice
+@RestControllerAdvice
 public class WriteExceptionHandler {
 
     public static final String INVALID_FIELD_MESSAGE = "Invalid field";
